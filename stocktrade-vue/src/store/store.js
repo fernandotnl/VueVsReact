96aa7afe4c1b7fe1bce9e13d8c8b3d01e91897c2
Fernando Tavares <fernandotnl@hotmail.com>
2017-09-19 22:58:48 -0300

    changing stores to be more like mobx

--- stocktrade-vue/src/store/store.js
+++ stocktrade-vue/src/store/store.js
@@ -4,7 +4,31 @@ import Vuex from 'vuex';
 import stocks from './modules/stocks';
 import portfolio from './modules/portfolio';
 
-import * as actions from './actions';
+const actions = {
+	loadData: ({commit}) => {
+	    Vue.http.get('data.json')
+	        .then(response => response.json())
+	        .then(data => {
+	            if (data) {
+	                const stocks = data.stocks;
+	                const funds = data.funds;
+	                const stockPortfolio = data.stockPortfolio;
+
+	                const portfolio = {
+	                    stockPortfolio,
+	                    funds
+	                };
+
+	                commit('SET_STOCKS', stocks);
+	                commit('SET_PORTFOLIO', portfolio);
+	            }
+	        });
+	},
+	resetData:({dispatch, commit}) => {
+	    dispatch('initStocks');
+	    dispatch('initPortfolio');
+	}
+}
 
 Vue.use(Vuex);
 
